5a5b901d0f1488b6a679e9bb604f84baedf72ad4
Ben Schaechter <bpschaechter@gmail.com>
2014-05-01 13:57:04 -0700

    staggered loading

--- content.js
+++ content.js
@@ -7,18 +7,18 @@
   To combat this, we'll inject the code directly using this
   file.  These files also must exist in 
   web_accessible_resources in the manifest.json file.
+
+  As a result of loading in jquery, load in tweetmeme
 */
 
-
-var jq = document.createElement('script'),
-    tm = document.createElement('script');
-
+var jq = document.createElement('script');
+var tm = document.createElement('script');
 jq.src = chrome.extension.getURL('scripts/jquery-1.7.2.min.js');
 tm.src = chrome.extension.getURL('scripts/tweetmeme.js');
 
 jq.onload = function() {
-    this.parentNode.removeChild(this);
-};
+  this.parentNode.removeChild(this);
+  document.head.appendChild(tm);
+}
 
-(document.head || document.documentElement).appendChild(jq);
-(document.head || document.documentElement).appendChild(tm);
+document.head.appendChild(jq);
